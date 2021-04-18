1023ee6148845ea74ba8bbd99ec345dc4f812c11
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-04-18 21:28:12 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -76,17 +76,3 @@ setInterval("showTime()", 1000);
 function p(s) {
     return s < 10 ? '0' + s : s;
 }
-
-// 心知天气
-(function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
-window.SeniverseWeatherWidget('show', {
-    flavor: "bubble", //bubble or slim
-    location: "WX4FBXXFKE4F",
-    geolocation: true,
-    language: "auto",
-    unit: "c",
-    theme: "auto",
-    token: "93ca93c8-a4a6-4a83-a8a9-823f1900a7cf",
-    hover: "enabled",
-    container: "tp-weather-widget"
-})
