aa66bac538f7aebd2e0d3956ada4519d4138c45a
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-04-18 21:23:06 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -59,3 +59,34 @@ document.getElementById('searchInput').onkeydown=function(e){
         location.href=url+$(this).val();
     }
 }
+
+// 显示时间
+function showTime() {
+    nowtime = new Date();
+    hour = nowtime.getHours();//时
+    minutes = nowtime.getMinutes(); //分
+    //seconds = nowtime.getSeconds(); //秒
+    //文字增加空格
+    document.getElementById("timer").style = "white-space:pre;";
+    //显示时间
+    document.getElementById("timer").innerText = p(hour) + ":" + p(minutes) /*+ ":" + p(seconds)*/;
+}
+setInterval("showTime()", 1000);
+//月日时分秒小于10补0
+function p(s) {
+    return s < 10 ? '0' + s : s;
+}
+
+// 心知天气
+(function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
+window.SeniverseWeatherWidget('show', {
+    flavor: "bubble", //bubble or slim
+    location: "WX4FBXXFKE4F",
+    geolocation: true,
+    language: "auto",
+    unit: "c",
+    theme: "auto",
+    token: "93ca93c8-a4a6-4a83-a8a9-823f1900a7cf",
+    hover: "enabled",
+    container: "tp-weather-widget"
+})
