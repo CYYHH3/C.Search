dd069a4ec61e9540f5a23833a36c6b3fff22f913
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-04-20 11:30:59 +0800

    Update console.js

--- js/console.js
+++ js/console.js
@@ -1,2 +1,1 @@
-console.log("%c\n", "font-size:41px;background:url('https://www.csearch.tk/img/index-icon.png') no-repeat");
-console.log("%c\n       ", "font-size:41px;background:url('http://cdn.iknow.bdimg.com/static/common/pkg/module_zed9cd9fd.png') no-repeat -135px -1px");
+console.log("%cCSearch", "colour: orange;")
