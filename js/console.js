ff563e4902ec15898badd8d044469910bb174480
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-04-20 11:02:47 +0800

    Update console.js

--- js/console.js
+++ js/console.js
@@ -1,2 +1,2 @@
-console.log("%c", "background:url('https://www.csearch.tk/img/index-icon.png')");
+console.log("%c\n", "font-size:41px;background:url('https://www.csearch.tk/img/index-icon.png')");
 console.log("%c\n       ", "font-size:41px;background:url('http://cdn.iknow.bdimg.com/static/common/pkg/module_zed9cd9fd.png') no-repeat -135px -1px");
