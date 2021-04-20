9a000f22938e060905becc7bbce4a77f85c33376
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-04-20 12:03:31 +0800

    Update console.js

--- js/console.js
+++ js/console.js
@@ -1,2 +1,2 @@
 console.log("%cCSearch", "font-size: 30px; color: #FD6B35;")
-console.log("%c\n\n\n\n\n", "background-image: url(https://www.csearch.tk/img/index-icon.png)")
+console.log("Copyright © 2021 CYY. All rights reserved.")
