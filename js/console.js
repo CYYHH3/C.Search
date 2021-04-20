21e88ea707c9700268bdb2b4d51b7b0d65b75cd4
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-04-20 11:54:52 +0800

    Update console.js

--- js/console.js
+++ js/console.js
@@ -1,2 +1,2 @@
 console.log("%cCSearch", "font-size: 30px; color: #FD6B35;")
-console.log("%c", "width: 100%; height: 100%; background-image: url(https://www.csearch.tk/img/index-icon.png)")
+console.log("%c\n\n\n\n\n", "background-image: url(https://www.csearch.tk/img/index-icon.png)")
