23b2d0dca1666670b5ac295fb8ded0e2e15176bb
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-04-20 12:10:29 +0800

    Update console.js

--- js/console.js
+++ js/console.js
@@ -1,2 +1,1 @@
-console.log("%cCSearch", "font-size: 30px; color: #FD6B35;")
-console.log("Copyright © 2021 CYY. All rights reserved.")
+console.log("%cCSearch\n\n", "font-family: 微软雅黑; font-size: 30px; color: #FD6B35;", "%cCopyright © 2021 CYY. All rights reserved.", "font-family: 微软雅黑;")
