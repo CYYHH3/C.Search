91158d51cf776744a02d1754b406a4cd5fd9a6d2
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-08-06 10:57:47 +0800

    Update console.js

--- js/console.js
+++ js/console.js
@@ -1,1 +1,1 @@
-console.log("%cC·Search\n%c欢迎来到简 · 搜索！\n%cCopyright © 2021 CYY. All rights reserved.", "font-family: 微软雅黑; font-size: 35px; color: #FD6B35; line-height: 3.5rem;", "font-family: 微软雅黑;", "font-family: 微软雅黑; line-height: 1.5rem")
+console.log("%cC·Search\n%c欢迎来到简 · 搜索！\n%cCopyright © 2021 CYY. All rights reserved.", "font-family: 微软雅黑, sans-serif; font-size: 35px; color: #FD6B35; line-height: 3.5rem;", "font-family: 微软雅黑, sans-serif;", "font-family: 微软雅黑, sans-serif; line-height: 1.5rem")
