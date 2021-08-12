9d440c0076387893683617e7e90355f4819ccf6d
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-08-12 15:31:43 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ window.SeniverseWeatherWidget('show', {
 WIDGET = {
     "CONFIG": {
         "modules": "012",
-        "background": "1", // 1 or 5
+        "background": "5", // 1 or 5
         "tmpColor": "FFFFFF",
         "tmpSize": "15",
         "cityColor": "FFFFFF",
