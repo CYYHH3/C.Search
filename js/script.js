dfc88c179d589a67498607743b76f87dd84112da
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-08-12 15:21:35 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -68,7 +68,7 @@ Waves.init();
 // 心知天气
 (function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
 window.SeniverseWeatherWidget('show', {
-    flavor: "bubble", //bubble or slim
+    flavor: "bubble", // bubble or slim
     location: "WX4FBXXFKE4F",
     geolocation: true,
     language: "auto",
@@ -83,7 +83,7 @@ window.SeniverseWeatherWidget('show', {
 WIDGET = {
     "CONFIG": {
         "modules": "012",
-        "background": "5",
+        "background": "1", // 1 or 5
         "tmpColor": "FFFFFF",
         "tmpSize": "15",
         "cityColor": "FFFFFF",
